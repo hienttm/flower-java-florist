8ab107b95865d4f612059660238ec819a3d32e91
Hien Tran <109644638+hienttm@users.noreply.github.com>
2023-12-09 13:58:37 +0700

    up

---
+++ JavaFlorist/JavaFlorist/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JavaFlorist.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
